e81c2e1b5b99ed0c5c2367933770eb3911cfcd41
Abbe Keultjes <flauwekeul@gmail.com>
2016-06-23 21:39:31 +0200

    Expanded Point to accept 1 or 2 numbers, an array or an object as parameters. Also added tests for Point.

--- src/point/index.js
+++ src/point/index.js
@@ -1,3 +1,24 @@
-export default function Point(x, y) {
-    return { x, y }
+import { isNumber, isObject, isArray } from '../utils'
+
+export default function Point(coordinatesOrX, y) {
+    // don't know how to make this more elegant **and** not fool babel
+    // e.g.: when replacing the last 3 return statements with a single
+    // return statement outside the if/else blocks, babel loses reference
+    // to x and y.
+    if (isNumber(coordinatesOrX)) {
+        const x = coordinatesOrX
+        return {
+            x,
+            y: isNumber(y) ? y : x
+        }
+    } else if (isArray(coordinatesOrX)) {
+        const [ x, y ] = coordinatesOrX
+        return { x, y }
+    } else if (isObject(coordinatesOrX)) {
+        const { x, y } = coordinatesOrX
+        return { x, y }
+    } else {
+        const x = y = 0
+        return { x, y }
+    }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,17 @@
-export function unsignNegativeZero(value) {
-    return isNumber(value) ? (value || 0) : value
-}
-
 export function isNumber(value) {
     return !isNaN(parseFloat(value)) && isFinite(value)
 }
 
 export function isObject(value) {
     return value !== null && typeof value === 'object'
+}
+
+export function isArray(value) {
+    return Array.isArray(value)
+}
+
+export function unsignNegativeZero(value) {
+    return isNumber(value) ? (value || 0) : value
 }
 
 export function stringToDOMNodes(string) {
