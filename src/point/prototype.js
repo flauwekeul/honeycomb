250ead6e7ba313a470d1bbfd5b6d1a8824b7f6cd
Abbe Keultjes <flauwekeul@gmail.com>
2016-07-03 22:52:20 +0200

    Added `multiply()`, `divide()` and `invert()` to Point.

--- src/point/prototype.js
+++ src/point/prototype.js
@@ -7,5 +7,17 @@ export default {
 
     subtract(point) {
         return Point(this.x - point.x, this.y - point.y)
+    },
+
+    multiply(point) {
+        return Point(this.x * point.x, this.y * point.y)
+    },
+
+    divide(point) {
+        return Point(this.x / point.x, this.y / point.y)
+    },
+
+    invert() {
+        return this.multiply(Point(-1))
     }
 }
--- test/point/prototype.spec.js
+++ test/point/prototype.spec.js
@@ -15,4 +15,25 @@ describe('Point prototype', () => {
             expect(result).to.contain({ x: -1, y: -7 })
         })
     })
+
+    describe('multiply', () => {
+        it('multiplies the coordinates of given point by itself', () => {
+            const result = Point(1, -3).multiply(Point(2, 4))
+            expect(result).to.contain({ x: 2, y: -12 })
+        })
+    })
+
+    describe('divide', () => {
+        it('divides the coordinates of given point by itself', () => {
+            const result = Point(1, -3).divide(Point(2, 4))
+            expect(result).to.contain({ x: 0.5, y: -0.75 })
+        })
+    })
+
+    describe('invert', () => {
+        it('negates/inverts itself', () => {
+            const result = Point(1, -3).invert()
+            expect(result).to.contain({ x: -1, y: 3 })
+        })
+    })
 })
