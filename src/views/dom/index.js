e0c86cb486582b4f64c00e5bf285a4805b48fc23
Abbe Keultjes <flauwekeul@gmail.com>
2016-07-06 07:59:11 +0200

    Improved setting of defaults for Views.DOM 🤓

--- src/views/dom/index.js
+++ src/views/dom/index.js
@@ -4,7 +4,7 @@ import Hex from '../../hex'
 
 import prototype from './prototype'
 
-export default function DOM({ container, origin, hex } = { origin: Point(0, 0) }) {
+export default function DOM({ container, hex, origin = Point(0, 0) }) {
     if (!isDom(container)) {
         console.warn(`Container is not a valid dom node: ${container}.`)
         return false
