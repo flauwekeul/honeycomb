c8b414a53aeca6cf51bf89313854788292f4e777
Abbe Keultjes <flauwekeul@gmail.com>
2017-04-05 15:52:56 +0200

    WIP: Wrap `DOM` in a `DOMFactory` to make testing easier (possible). Update dom view creation to work with the earlier changes. I might have coupled the tests too tightly to the implementation...but I'll fix that when the time comes...

--- src/views/dom.js
+++ src/views/dom.js
@@ -1,86 +1,79 @@
-import isDom from 'is-dom'
-import Point from '../point'
-import HexFactory from '../hex'
+export default function DOMFactory({ Hex = HexFactory(), Point, isDom } = {}) {
+    /**
+     * @function Views.DOM
+     *
+     * @description
+     * Factory function for creating a DOM view object. This object can be used to render an array of hexes it's passed or a grid instance. If a grid instance is passed, its {@link Grid#colSize|colSize} and {@link Grid#rowSize|rowSize} is used to calculate how many hexes are to be shown in a rectangular shape.
+     *
+     * @param {Object} options           An options object.
+     * @param {Node} options.container   A DOM node to render hexes in.
+     * @param {Object} options.hex       An options object containing a `hex` property with an `element` property that get's passed to {@link Hex#element}.
+     * @param {Point} options.origin     A point where the first hex (i.e. `Hex(0, 0, 0)`) can be rendered.
+     *
+     * @returns {Object}            An object with helper methods like to render a view for a hex grid using the DOM.
+     */
+    return function DOM({ container, origin = 0 } = {}) {
+        if (!isDom(container)) {
+            throw new Error(`Container is not a valid dom node: ${container}.`)
+        }
 
-const Hex = HexFactory()
+        return {
+            container,
+            origin: Point(origin),
 
-/**
- * @function Views.DOM
- *
- * @description
- * Factory function for creating a DOM view object. This object can be used to render an array of hexes it's passed or a grid instance. If a grid instance is passed, its {@link Grid#colSize|colSize} and {@link Grid#rowSize|rowSize} is used to calculate how many hexes are to be shown in a rectangular shape.
- *
- * @param {Object} options           An options object.
- * @param {Node} options.container   A DOM node to render hexes in.
- * @param {Object} options.hex       An options object containing a `hex` property with an `element` property that get's passed to {@link Hex#element}.
- * @param {Point} options.origin     A point where the first hex (i.e. `Hex(0, 0, 0)`) can be rendered.
- *
- * @returns {Object}            An object with helper methods like to render a view for a hex grid using the DOM.
- */
-export default function DOM({ container, hex, origin = Point(0, 0) }) {
-    if (!isDom(container)) {
-        console.warn(`Container is not a valid dom node: ${container}.`)
-        return false
-    }
+            /**
+             * @method Views.DOM#render
+             *
+             * @description
+             * Renders the passed {@link Grid|grid} instance in the container. The container is completely covered with hexes.
+             *
+             * @todo validate `grid`
+             *
+             * @param   {Object} grid   A grid instance (from the {@link Grid} factory).
+             *
+             * @returns {Object}        The DOM View object, for chaining.
+             */
+            render(grid) {
+                // increase the size of the hex rectangle to guarantee it covers the container
+                const width = Math.round(this.container.offsetWidth / grid.colSize()) + 3
+                const height = Math.round(this.container.offsetHeight / grid.rowSize()) + 3
+                const start = Hex.subtract(grid.pointToHex(this.origin.invert()), Hex(1))
 
-    Hex.element(hex.element)
+                return this.renderHexes(grid.rectangle(width, height, start))
+            },
 
-    return {
-        origin: Point(origin),
-        container,
-        hex,
+            /**
+             * @method Views.DOM#renderHexes
+             *
+             * @description
+             * Renders the passed hexes in the container.
+             *
+             * @param   {Hex[]} hexes   An array of hexes to render.
+             *
+             * @returns {Object}        The DOM View object, for chaining.
+             */
+            renderHexes(hexes) {
+                const hexNodes = hexes.reduce((fragment, hex) => {
+                    const hexNode = stringToDOMNodes(hex.view())[0]
+                    const hexOffset = this.origin.add(hex.toPoint())
 
-        /**
-         * @method Views.DOM#render
-         *
-         * @description
-         * Renders the passed {@link Grid|grid} instance in the container. The container is completely covered with hexes.
-         *
-         * @param   {Object} grid   A grid instance (from the {@link Grid} factory).
-         *
-         * @returns {Object}        The DOM View object, for chaining.
-         */
-        render(grid) {
-            // increase the size of the hex rectangle to guarantee it covers the container
-            const width = Math.round(this.container.offsetWidth / grid.colSize()) + 3
-            const height = Math.round(this.container.offsetHeight / grid.rowSize()) + 3
-            const start = grid.pointToHex(this.origin.invert()).subtract(Hex(1))
+                    Object.assign(hexNode.style, {
+                        position: 'absolute',
+                        width: `${hex.width()}px`,
+                        height: `${hex.height()}px`,
+                        left: `${hexOffset.x}px`,
+                        top: `${hexOffset.y}px`
+                    })
 
-            return this.renderHexes(grid.rectangle(width, height, start))
-        },
+                    fragment.appendChild(hexNode)
 
-        /**
-         * @method Views.DOM#renderHexes
-         *
-         * @description
-         * Renders the passed hexes in the container.
-         *
-         * @param   {Hex[]} hexes   An array of hexes to render.
-         *
-         * @returns {Object}        The DOM View object, for chaining.
-         */
-        renderHexes(hexes) {
-            const style = {
-                position: 'absolute',
-                width: `${Hex.width()}px`,
-                height: `${Hex.height()}px`
+                    return fragment
+                }, document.createDocumentFragment())
+
+                this.container.appendChild(hexNodes)
+
+                return this
             }
-
-            const hexNodes = hexes.reduce((fragment, hex) => {
-                const hexNode = stringToDOMNodes(hex.element())[0]
-                const hexOffset = this.origin.add(hex.toPoint())
-
-                Object.assign(hexNode.style, style, {
-                    left: `${hexOffset.x}px`,
-                    top: `${hexOffset.y}px`
-                })
-
-                fragment.appendChild(hexNode)
-                return fragment
-            }, document.createDocumentFragment())
-
-            this.container.appendChild(hexNodes)
-            return this
         }
     }
 }
--- test/views/dom.spec.js
+++ test/views/dom.spec.js
@@ -2,144 +2,117 @@ import { expect } from 'chai'
 import sinon from 'sinon'
 import jsdom from 'mocha-jsdom'
 
-import Hex from '../../src/hex'
+import HexFactory from '../../src/hex'
 import Grid from '../../src/grid'
-import DOM from '../../src/views/dom'
-
-let grid, container, dom
+import DOMFactory from '../../src/views/dom'
 
 describe('DOM View creation', () => {
-    jsdom()
-    before(() => sinon.spy(Hex, 'element'))
-    after(() => Hex.element.restore())
+    const Hex = sinon.stub()
+    const Point = sinon.stub().callsFake(value => value)
+    const isDom = sinon.stub().returns(true)
+    const DOM = DOMFactory({ Hex, Point, isDom })
 
-    describe('with valid options', () => {
-        const hexElement = '<div></div>'
-
-        it('returns a DOM view with the passed options', () => {
-            const container = document.createElement('div')
-            const view = DOM({
-                container: container,
-                origin: [6, 3],
-                hex: { element: hexElement }
-            })
-            expect(view).to.have.property('container').that.eqls(container)
-            expect(view).to.have.property('origin').that.contains({ x: 6, y: 3 })
-            expect(view).to.have.property('hex').that.eqls({ element: hexElement })
-        })
-
-        it('sets Hex.element', () => {
-            expect(Hex.element).to.have.been.calledWith(hexElement)
-        })
+    it('calls isDom to check if the container is a valid DOM node', () => {
+        const container = 'container'
+        DOM({ container })
+        expect(isDom).to.have.been.calledWith(container)
     })
 
-    describe('with invalid options', () => {
-        describe('invalid container', () => {
-            before(() => sinon.spy(console, 'warn'))
-            after(() => console.warn.restore())
+    it('throws an error if the container is not a valid DOM node', () => {
+        isDom.returns(false)
+        expect(DOM).to.throw('Container is not a valid dom node: undefined.')
+    })
 
-            it('shows a warning', () => {
-                DOM({})
-                expect(console.warn).to.have.been.calledWith('Container is not a valid dom node: undefined.')
-                DOM({ container: '<div></div>' })
-                expect(console.warn).to.have.been.calledWith('Container is not a valid dom node: <div></div>.')
-            })
+    describe('with valid options', () => {
+        const container = 'valid container'
+        const origin = [2, 3]
+
+        before(() => isDom.returns(true))
+
+        it('wraps the passed origin in a Point', () => {
+            DOM({ origin })
+            expect(Point).to.have.been.calledWith(origin)
+        })
+
+        it('returns a DOM View instance', () => {
+            const result = DOM({ container, origin })
+
+            expect(result).to.have.property('container').that.eqls(container)
+            expect(result).to.have.property('origin').that.eqls(origin)
+            expect(result).to.have.property('render').that.is.a('function')
+            expect(result).to.have.property('renderHexes').that.is.a('function')
         })
     })
 })
 
 describe('DOM View rendering', () => {
-    jsdom()
-
-    beforeEach(() => {
-        grid = Grid({
-            hex: {
-                orientation: 'pointy',
-                size: 10,
-                origin: [0, 0]
-            }
-        })
-        container = document.createElement('div')
-        dom = DOM({
-            container: container,
-            hex: { element: '<div></div>' }
-        })
-        sinon.spy(dom, 'renderHexes')
-        sinon.spy(grid, 'rectangle')
-    })
-
-    afterEach(() => {
-        dom.renderHexes.restore()
-        grid.rectangle.restore()
-    })
-
-    describe('renderHexes', () => {
-        it('renders the passed hexes in the container', () => {
-            const hexes = [ Hex(0, 0), Hex(1, 0) ]
-
-            dom.renderHexes(hexes)
-            expect(container.childNodes).to.have.length(2)
-        })
-
-        it('sets each hex\'s style properties position, width and height the same', () => {
-            const hexes = [ Hex(0, 0), Hex(1, 0) ]
-
-            dom.renderHexes(hexes)
-
-            const renderedHex1 = container.childNodes[0]
-            const renderedHex2 = container.childNodes[1]
-            const expected = {
-                position: 'absolute',
-                width: `${Hex.width()}px`,
-                height: `${Hex.height()}px`
-            }
-
-            expect(renderedHex1.style).to.contain(expected)
-            expect(renderedHex2.style).to.contain(expected)
-        })
-
-        it('sets each hex\'s style properties top and left', () => {
-            const hexes = [ Hex(1, 0), Hex(0, 1) ]
-
-            dom.renderHexes(hexes)
-
-            const renderedHex1Style = container.childNodes[0].style
-            const renderedHex2Style = container.childNodes[1].style
-
-            expect(renderedHex1Style).to.have.property('top').that.equals('0px')
-            expect(renderedHex1Style).to.have.property('left').that.equals(`${Hex.width()}px`)
-            expect(renderedHex2Style).to.have.property('top').that.equals(`${grid.rowSize()}px`)
-            expect(renderedHex2Style).to.have.property('left').that.equals(`${Hex.width() / 2}px`)
-        })
-    })
+    const subtract = sinon.stub().returns('subtract result')
+    const Hex = sinon.stub().returns('Hex result')
+    Hex.subtract = subtract
+    const Point = sinon.stub().callsFake(value => value)
+    const isDom = sinon.stub().returns(true)
+    const DOM = DOMFactory({ Hex, Point, isDom })
 
     describe('render', () => {
         it('renders a rectangle from the passed grid', () => {
-            // mock container offsetWidth and offsetHeight because there's no setter
-            // https://github.com/tmpvar/jsdom/issues/135#issuecomment-68191941
-            Object.defineProperties(container, {
-                offsetWidth: { value: 30 },
-                offsetHeight: { value: 30 }
-            })
-
-            dom.render(grid)
-
-            const expectedStart = Hex(-1).coordinates()
-
-            function expectedGridRectangleResult(actualHexes) {
-                const expectedHexes = grid.rectangle(5, 5, expectedStart)
-
-                // every actual hex...
-                return actualHexes.every(actualHex => {
-                    // ...should equal at least 1 expected hex
-                    return expectedHexes.some(expectedHex => {
-                        return JSON.stringify(actualHex) === JSON.stringify(expectedHex)
-                    })
-                })
+            const container = {
+                offsetWidth: 1,
+                offsetHeight: 1
             }
+            const invert = sinon.stub().returns('invert result')
+            const origin = { invert }
+            const dom = DOM()
+            const colSize = sinon.stub().returns(1)
+            const rowSize = sinon.stub().returns(1)
+            const pointToHex = sinon.stub().returns('pointToHex result')
+            const rectangle = sinon.stub().returns('rectangle result')
+            const grid = { colSize, rowSize, pointToHex, rectangle }
+            const renderHexes = sinon.stub().returns('renderHexes result')
+            const result = dom.render.bind({ container, origin, renderHexes })(grid)
 
-            expect(grid.rectangle).to.have.been.calledWith(5, 5, sinon.match(expectedStart))
-            expect(dom.renderHexes).to.have.been.calledWith(sinon.match(expectedGridRectangleResult, 'array of hexes'))
+            expect(colSize).to.have.been.called
+            expect(rowSize).to.have.been.called
+            expect(Hex).to.have.been.calledWith(1)
+            expect(invert).to.have.been.called
+            expect(pointToHex).to.have.been.calledWith('invert result')
+            expect(subtract).to.have.been.calledWith('pointToHex result', 'Hex result')
+            expect(rectangle).to.have.been.calledWith(4, 4, 'subtract result')
+            expect(renderHexes).to.have.been.calledWith('rectangle result')
+            expect(result).to.equal('renderHexes result')
+        })
+    })
+
+    describe('renderHexes', () => {
+        jsdom()
+
+        it('renders a rectangle from the passed grid', () => {
+            const container = document.createElement('div')
+            const add = sinon.stub().returns({ x: 1, y: 2 })
+            const origin = { add }
+            const dom = DOM()
+            const view = sinon.stub().returns('<div></div>')
+            const toPoint = sinon.stub().returns('toPoint result')
+            const width = sinon.stub().returns(3)
+            const height = sinon.stub().returns(4)
+            const hexes = [{ view, toPoint, width, height }]
+            const context = { container, origin }
+            const result = dom.renderHexes.bind(context)(hexes)
+
+            expect(view).to.have.been.called
+            expect(toPoint).to.have.been.called
+            expect(add).to.have.been.calledWith('toPoint result')
+            expect(width).to.have.been.called
+            expect(height).to.have.been.called
+            expect(result).to.eql(context)
+
+            const hexNode = container.firstChild
+
+            expect(hexNode.nodeName).to.equal('DIV')
+            expect(hexNode.style).to.have.property('position', 'absolute')
+            expect(hexNode.style).to.have.property('left', '1px')
+            expect(hexNode.style).to.have.property('top', '2px')
+            expect(hexNode.style).to.have.property('width', '3px')
+            expect(hexNode.style).to.have.property('height', '4px')
         })
     })
 })
