0a6155b204dff2793ff1e1519b5e23a9472949cb
Abbe Keultjes <flauwekeul@gmail.com>
2017-05-07 21:50:16 +0200

    Make honeycomb test file end in `.spec.js`.

--- src/utils.js
+++ src/utils.js
@@ -19,10 +19,3 @@ export const is = {
 export function unsignNegativeZero(value) {
     return is.number(value) ? (value || 0) : value
 }
-
-/*eslint-env browser*/
-export function stringToDOMNodes(string) {
-    const div = document.createElement('div')
-    div.innerHTML = string.trim()
-    return div.children
-}
