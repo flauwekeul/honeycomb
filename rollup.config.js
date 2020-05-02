6425e92efa06667432a598a2c7cd4e0d56e43241
Abbe Keultjes <flauwekeul@gmail.com>
2020-05-02 22:50:09 +0200

    style: add prettier and format all files

--- rollup.config.js
+++ rollup.config.js
@@ -8,31 +8,31 @@ import { terser } from 'rollup-plugin-terser'
 const isProduction = process.env.NODE_ENV === 'production'
 
 export default {
-    input: 'src/honeycomb.js',
-    output: [
-        {
-            name: 'Honeycomb',
-            format: 'umd',
-            file: `dist/honeycomb${isProduction ? '.min' : ''}.js`,
-            sourcemap: true
-        },
-        {
-            name: 'Honeycomb',
-            format: 'es',
-            file: `dist/honeycomb.esm${isProduction ? '.min' : ''}.js`,
-            sourcemap: true
-        }
-    ],
-    plugins: [
-        babel({
-            exclude: 'node_modules/**'
-        }),
-        resolve(),
-        commonjs({
-            namedExports: {
-                'axis.js': ['isObject', 'isNumber', 'isArray', 'isString']
-            }
-        }),
-        ...(isProduction ? [terser()] : [])
-    ]
+  input: 'src/honeycomb.js',
+  output: [
+    {
+      name: 'Honeycomb',
+      format: 'umd',
+      file: `dist/honeycomb${isProduction ? '.min' : ''}.js`,
+      sourcemap: true,
+    },
+    {
+      name: 'Honeycomb',
+      format: 'es',
+      file: `dist/honeycomb.esm${isProduction ? '.min' : ''}.js`,
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+    }),
+    resolve(),
+    commonjs({
+      namedExports: {
+        'axis.js': ['isObject', 'isNumber', 'isArray', 'isString'],
+      },
+    }),
+    ...(isProduction ? [terser()] : []),
+  ],
 }
