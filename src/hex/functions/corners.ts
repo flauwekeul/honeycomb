9c3d240134909807be890d139936e406c9e0bf03
Abbe Keultjes <flauwekeul@gmail.com>
2020-05-25 22:46:57 +0200

    improvement(hex): corners() returns corners relative to origin instead of top left

--- src/hex/functions/corners.ts
+++ src/hex/functions/corners.ts
@@ -1,27 +1,29 @@
 import { Hex, HexSettings, Orientation, Point } from '../types'
 import { heightFlat, heightPointy } from './height'
+import { hexToPoint } from './hexToPoint'
 import { widthFlat, widthPointy } from './width'
 
 export const cornersPointy = (width: number, height: number, { x, y }: Point) => [
-  { x: width - x, y: height * 0.25 - y },
-  { x: width - x, y: height * 0.75 - y },
-  { x: width * 0.5 - x, y: height - y },
-  { x: 0 - x, y: height * 0.75 - y },
-  { x: 0 - x, y: height * 0.25 - y },
-  { x: width * 0.5 - x, y: 0 - y },
+  { x: x + width * 0.5, y: y - height * 0.25 },
+  { x: x + width * 0.5, y: y + height * 0.25 },
+  { x, y: y + height * 0.5 },
+  { x: x - width * 0.5, y: y + height * 0.25 },
+  { x: x - width * 0.5, y: y - height * 0.25 },
+  { x, y: y - height * 0.5 },
 ]
 
 export const cornersFlat = (width: number, height: number, { x, y }: Point) => [
-  { x: width - x, y: height * 0.5 - y },
-  { x: width * 0.75 - x, y: height - y },
-  { x: width * 0.25 - x, y: height - y },
-  { x: 0 - x, y: height * 0.5 - y },
-  { x: width * 0.25 - x, y: 0 - y },
-  { x: width * 0.75 - x, y: 0 - y },
+  { x: x + width * 0.25, y: y - height * 0.5 },
+  { x: x + width * 0.5, y },
+  { x: x + width * 0.25, y: y + height * 0.5 },
+  { x: x - width * 0.25, y: y + height * 0.5 },
+  { x: x - width * 0.5, y },
+  { x: x - width * 0.25, y: y - height * 0.5 },
 ]
 
+// todo: use separate functions for absolute and relative corners?
 export const corners = ({ orientation, dimensions: { xRadius, yRadius }, origin }: HexSettings, hex?: Hex): Point[] => {
-  const point = hex ? hex.toPoint() : origin
+  const point = hex ? hexToPoint(hex) : origin
   return orientation === Orientation.POINTY
     ? cornersPointy(widthPointy(xRadius), heightPointy(yRadius), point)
     : cornersFlat(widthFlat(xRadius), heightFlat(yRadius), point)
