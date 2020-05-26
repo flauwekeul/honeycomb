0db666d52c12d4598f963a0810c492d17660616e
Abbe Keultjes <flauwekeul@gmail.com>
2020-05-26 12:14:48 +0200

    feat(hex): set hex origin relative to center of hex
    It's also possible to set an origin with a function. This function is called with the hex prototype so that width, height, etc can be used. Also, hexToPoint() now subtracts the origin.

--- src/hex/functions/hexToPoint.ts
+++ src/hex/functions/hexToPoint.ts
@@ -1,12 +1,12 @@
 import { Hex, Orientation, Point } from '../types'
 
-export const hexToPoint = ({ orientation, dimensions: { xRadius, yRadius }, q, r }: Hex) =>
+export const hexToPoint = ({ orientation, dimensions: { xRadius, yRadius }, origin: { x, y }, q, r }: Hex) =>
   orientation === Orientation.POINTY
     ? ({
-        x: xRadius * Math.sqrt(3) * (q + r / 2),
-        y: ((yRadius * 3) / 2) * r,
+        x: xRadius * Math.sqrt(3) * (q + r / 2) - x,
+        y: ((yRadius * 3) / 2) * r - y,
       } as Point)
     : ({
-        x: ((xRadius * 3) / 2) * q,
-        y: yRadius * Math.sqrt(3) * (r + q / 2),
+        x: ((xRadius * 3) / 2) * q - x,
+        y: yRadius * Math.sqrt(3) * (r + q / 2) - y,
       } as Point)
