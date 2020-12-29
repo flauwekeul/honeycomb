191eb70c5490415c6e383029ddc84f809f722c5c
Abbe Keultjes <flauwekeul@gmail.com>
2020-12-29 21:25:11 +0100

    build: upgrade dependencies
    Also remove documentation dependency.

--- src/utils/isObject.ts
+++ src/utils/isObject.ts
@@ -1,1 +1,2 @@
-export const isObject = <T = {}>(value: unknown): value is T => typeof value === 'object' && value !== null
+export const isObject = <T = Record<string, unknown>>(value: unknown): value is T =>
+  typeof value === 'object' && value !== null
