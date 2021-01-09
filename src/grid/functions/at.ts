fbde28c487dc0ca7be6fda8af939b80e4c8e5780
Abbe Keultjes <flauwekeul@gmail.com>
2021-01-09 21:45:12 +0100

    feat(grid): traverse functions don't create hexes anymore
    Grid.traverse() makes hexes, this makes the traverse functions simpler.

--- src/grid/functions/at.ts
+++ src/grid/functions/at.ts
@@ -1,10 +1,15 @@
-import { createHex, Hex, HexCoordinates } from '../../hex'
+import { HexCoordinates } from '../../hex'
 import { Traverser } from '../types'
 
-export const at = <T extends Hex>(coordinates: HexCoordinates): Traverser<T> =>
-  function* next(currentHex) {
-    // todo: make createHex accept hex instances or use cloneHex()?
-    yield createHex(Object.getPrototypeOf(currentHex), coordinates)
+// export const at = <T extends Hex>(coordinates: HexCoordinates): Traverser<T> =>
+//   function* next(currentHex) {
+//     // todo: make createHex accept hex instances or use cloneHex()?
+//     yield createHex(Object.getPrototypeOf(currentHex), coordinates)
+//   }
+
+export const at = (coordinates: HexCoordinates): Traverser =>
+  function* next() {
+    yield coordinates
   }
 
 export const start = at
