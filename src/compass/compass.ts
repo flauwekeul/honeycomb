f3b6bb38ccd9eaf4e07a3340caf28d8bc6c8c927
Abbe Keultjes <flauwekeul@gmail.com>
2021-02-08 16:22:27 +0100

    feat(grid): rewrite the rectangle traverser and update Grid.rectangle() to use it, add Compass class
    The rectangle() traverser now uses other traversers (including the new concat() and branch()) to
    create a rectangle. This means a huge improvement in declarativeness and quite a performance
    improvement too! The only downside is that the new rectangle() traverser can't create rectangles
    with 90° corners for ordinal directions. I expect people won't use this feature anyway and I can
    always add it later. In order to make the new rectangle() traverser work, the Compass class is
    introduced. It offers a utility method to rotate a direction (e.g. rotating NE -3 steps yields W).
    Finally, Grid.rectangle() simply calls traverse(rectangle()), passes the options and returns the
    result.

---
+++ src/compass/compass.ts
@@ -0,0 +1,78 @@
+export enum CardinalCompassDirection {
+  N = 0,
+  E = 2,
+  S = 4,
+  W = 6,
+}
+
+export enum OrdinalCompassDirection {
+  NE = 1,
+  SE = 3,
+  SW = 5,
+  NW = 7,
+}
+
+export enum CompassDirection {
+  N,
+  NE,
+  E,
+  SE,
+  S,
+  SW,
+  W,
+  NW,
+}
+
+export type CompassDirectionLike = keyof typeof CompassDirection | number
+
+export class Compass {
+  static N = CompassDirection.N
+  static NE = CompassDirection.NE
+  static E = CompassDirection.E
+  static SE = CompassDirection.SE
+  static S = CompassDirection.S
+  static SW = CompassDirection.SW
+  static W = CompassDirection.W
+  static NW = CompassDirection.NW
+  static Cardinal: CardinalCompassDirection
+  static Ordinal: OrdinalCompassDirection
+
+  static of(direction: CompassDirectionLike = CompassDirection.N) {
+    return new Compass(direction)
+  }
+
+  static isCardinal(direction: CompassDirection) {
+    return !!CardinalCompassDirection[direction]
+  }
+
+  static isOrdinal(direction: CompassDirection) {
+    return !!OrdinalCompassDirection[direction]
+  }
+
+  static rotate(direction: number, steps: number): CompassDirection {
+    return signedModulo(direction + steps, 8)
+  }
+
+  direction: CompassDirection
+
+  constructor(direction: CompassDirectionLike = CompassDirection.N) {
+    this.direction = typeof direction === 'number' ? direction : CompassDirection[direction]
+  }
+
+  isCardinal() {
+    return Compass.isCardinal(this.direction)
+  }
+
+  isOrdinal() {
+    return Compass.isOrdinal(this.direction)
+  }
+
+  rotate(steps: number) {
+    return Compass.rotate(this.direction, steps)
+  }
+}
+
+// todo: move to utils?
+function signedModulo(dividend: number, divisor: number) {
+  return ((dividend % divisor) + divisor) % divisor
+}
