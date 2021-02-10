43d01092b6badaf88baaed6acb3aa33c9a6f1972
Abbe Keultjes <flauwekeul@gmail.com>
2021-02-10 22:12:50 +0100

    feat(hex): createHex() now also accepts a hex (instance) and if it does copies it
    It doesn't seem to have major performance impact and it improves the API

--- src/hex/functions/copyHex.ts
+++ src/hex/functions/copyHex.ts
@@ -1,4 +1,4 @@
 import { Hex, HexCoordinates } from '../types'
 
-export const copyHex = <T extends Hex>(hex: T, newProps: Partial<T> | HexCoordinates = {}) =>
+export const copyHex = <T extends Hex>(hex: T, newProps: Partial<T> | HexCoordinates = {}): T =>
   Object.assign(Object.create(Object.getPrototypeOf(hex)), hex, newProps)
