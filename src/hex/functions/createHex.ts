b792e021bb4a29a93140d659eda113f8c267c7fe
Abbe Keultjes <flauwekeul@gmail.com>
2021-02-10 22:12:50 +0100

    feat(hex): createHex() now also accepts a hex (instance) and if it does copies it
    It doesn't seem to have major performance impact and it improves the API

--- src/hex/functions/createHex.ts
+++ src/hex/functions/createHex.ts
@@ -1,10 +1,8 @@
 import { Hex, HexCoordinates } from '../types'
+import { isHex } from './isHex'
 
-export const createHex = <T extends Hex>(
-  prototype: T,
-  { q, r, s = -q - r, ...rest }: HexCoordinates | T = { q: 0, r: 0 },
-) =>
-  // fixme: when `prototype` is a hex instance, an object with that instance as prototype is created...
-  // either only accept a hex prototype or check if `prototype` is a hex instance and then clone it?
-  // todo: make coordinates readonly
-  Object.assign(Object.create(prototype), { q, r, s }, rest) as T extends Hex ? T : T & Hex
+export const createHex = <T extends Hex>(prototypeOrHex: T, props: HexCoordinates | T = { q: 0, r: 0 }): T =>
+  // if the prototype of the 1st argument is (that of) a hex, the 1st argument is a hex instance
+  isHex(Object.getPrototypeOf(prototypeOrHex))
+    ? prototypeOrHex.copy(props)
+    : Object.assign(Object.create(prototypeOrHex), props)
