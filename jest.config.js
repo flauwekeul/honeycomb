c0bf6b6ce2d865efe574ff7ab69a0327d7f3ee87
Abbe Keultjes <flauwekeul@gmail.com>
2021-02-15 20:57:37 +0100

    refactor: remove obsolete files

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  // temporary until src-js is removed
-  testPathIgnorePatterns: ['/node_modules/', '/src-js/'],
-}
--- test/config.js
+++
@@ -1,5 +0,0 @@
-import chai from 'chai'
-import sinonChai from 'sinon-chai'
-import { hexes } from './helpers'
-
-chai.use(sinonChai).use(hexes)
