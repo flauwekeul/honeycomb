fa30371a82fae8d6f1237968692a4f30a0ec0d59
Abbe Keultjes <flauwekeul@gmail.com>
2021-02-20 16:54:59 +0100

    refactor(grid): rename several things and remove "magic" where first traversal sets state
    Rules about what to do when traversing outside the grid doesn't belong in Grid's traverse() but
    should be implemented in its other methods (like filter() and takeWhile() that need to be made yet).
    A grid may be aware of state (in order to reuse hexes from that state), but how to use it should be
    left to the user as much as possible. Also, not having this magic where the first traversal is
    different from future traversals is needlessly complex.

--- src/grid/utils/map.ts
+++
@@ -1,7 +0,0 @@
-export const map = <T>(fn: (value: T) => T) => (iterable: Iterable<T>) => {
-  const result: T[] = []
-  for (const value of iterable) {
-    result.push(fn(value))
-  }
-  return result
-}
