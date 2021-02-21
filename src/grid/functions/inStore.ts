bb08ce81fa51de4655ca4ce8078d2e48e7f817b6
Abbe Keultjes <flauwekeul@gmail.com>
2021-02-21 21:52:37 +0100

    refactor(grid): make a Grid store just a regular Map, simplifying things significantly

--- src/grid/functions/inStore.ts
+++ src/grid/functions/inStore.ts
@@ -1,8 +1,5 @@
 import { Hex } from '../../hex'
 import { Grid } from '../grid'
-import { GridStore } from '../types'
 
-export const inStore = <T extends Hex, S extends GridStore<T>, G extends GridStore<T>>(store?: S) => (
-  hex: T,
-  grid: Grid<T, G>,
-): boolean => !!(store ?? grid.store)?.get(hex.toString())
+export const inStore = <T extends Hex>(store?: Map<string, T>) => (hex: T, grid: Grid<T>): boolean =>
+  !!(store ?? grid.store)?.get(hex.toString())
--- src/grid/functions/setStore.ts
+++ src/grid/functions/setStore.ts
@@ -1,8 +1,5 @@
 import { Hex } from '../../hex'
 import { Grid } from '../grid'
-import { GridStore } from '../types'
 
-export const setStore = <T extends Hex, S extends GridStore<T>, G extends GridStore<T>>(store?: S) => (
-  hex: T,
-  grid: Grid<T, G>,
-): GridStore<T> | void => (store ?? grid.store)?.set(hex.toString(), hex)
+export const setStore = <T extends Hex>(store?: Map<string, T>) => (hex: T, grid: Grid<T>) =>
+  (store ?? grid.store)?.set(hex.toString(), hex)
