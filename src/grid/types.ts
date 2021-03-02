e56ced817a3ed64e913352d30b5c819eff43e549
Abbe Keultjes <flauwekeul@gmail.com>
2021-03-02 15:44:14 +0100

    feat(grid): a grid can now be created with an optional traverser
    A grid without traverser is pretty useless. So an optional traverser can now be passed as the 2nd
    argument to the Grid constructor (or `Grid.of()`) and will be converted to internal "hex state".
    Made the clone method private because I don't think people will use it and it requires an internal
    function (GetHexState) is passed.

--- src/grid/types.ts
+++ src/grid/types.ts
@@ -1,15 +1,6 @@
 import { Hex, HexCoordinates } from '../hex'
 import { Grid } from './grid'
 
-export interface HexState<T extends Hex> {
-  hexes: T[]
-  cursor: T | null
-}
-
-export interface GetHexState<T extends Hex> {
-  (grid: Grid<T>): HexState<T>
-}
-
 export interface Traverser<T extends Hex> {
   (cursor: T, getHex: (coordinates: HexCoordinates) => T): Iterable<T>
 }
