ab8625adc884b56b3892b9e38b8ccae33fb27af7
Abbe Keultjes <flauwekeul@gmail.com>
2021-04-24 21:36:08 +0200

    refactor(grid): minor simplification in the add() traverser

--- src/grid/traversers/add.test.ts
+++ src/grid/traversers/add.test.ts
@@ -1,13 +1,12 @@
-import { Hex } from '../../hex'
+import { createHexPrototype, Hex } from '../../hex'
+import { Grid } from '../grid'
 import { add } from './add'
 
 test('accepts coordinates and returns a traverser that returns the hex with those coordinates', () => {
-  const getHex = jest.fn((coordinates) => coordinates)
+  const grid = new Grid(createHexPrototype())
 
-  expect(add({ q: 1, r: 2 }, { q: 3, r: 4 })({} as Hex, getHex)).toEqual([
+  expect(add({ q: 1, r: 2 }, { q: 3, r: 4 })({} as Hex, grid.getHex)).toMatchObject([
     { q: 1, r: 2 },
     { q: 3, r: 4 },
   ])
-  expect(getHex).toBeCalledWith({ q: 1, r: 2 })
-  expect(getHex).toBeCalledWith({ q: 3, r: 4 })
 })
--- src/grid/traversers/add.ts
+++ src/grid/traversers/add.ts
@@ -2,4 +2,4 @@ import { Hex, HexCoordinates } from '../../hex'
 import { Traverser } from '../types'
 
 export const add = <T extends Hex>(...coordinates: HexCoordinates[]): Traverser<T, T[]> => (_, getHex) =>
-  coordinates.map((coords) => getHex(coords))
+  coordinates.map(getHex)
