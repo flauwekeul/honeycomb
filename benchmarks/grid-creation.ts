f4b65f8b35d47f33dcd28d0aca59880233ddaab2
Abbe Keultjes <flauwekeul@gmail.com>
2022-07-28 21:25:51 +0200

    docs(benchmarks): add benchmarks folder and script to run them

---
+++ benchmarks/grid-creation.ts
@@ -0,0 +1,20 @@
+import { add, cycle, suite } from 'benny'
+import { createHexPrototype, Grid, rectangle } from '../dist'
+
+const hexPrototype = createHexPrototype({
+  dimensions: 30,
+})
+
+suite(
+  'Grid creation',
+  add('10⨉10 rectangle', () => {
+    new Grid(hexPrototype, rectangle({ width: 10, height: 10 }))
+  }),
+  add('100⨉100 rectangle', () => {
+    new Grid(hexPrototype, rectangle({ width: 100, height: 100 }))
+  }),
+  add('1000⨉1000 rectangle', () => {
+    new Grid(hexPrototype, rectangle({ width: 1000, height: 1000 }))
+  }),
+  cycle(),
+)
