760113e162d3475a043cee018ded9d77b1c95522
Abbe Keultjes <flauwekeul@gmail.com>
2022-07-30 12:01:53 +0200

    :wrench: update eslint config to be more strict
    ignore tests for now

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,28 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'jest'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-    'prettier',
+  overrides: [
+    {
+      files: ['*.ts'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/strict',
+      ],
+      parserOptions: {
+        project: ['./tsconfig.json'],
+      },
+      rules: {
+        '@typescript-eslint/unified-signatures': ['off'],
+        '@typescript-eslint/no-unnecessary-condition': ['off'],
+      },
+    },
+    {
+      files: ['*.test.ts'],
+      extends: ['plugin:jest/recommended'],
+    },
   ],
+  extends: ['eslint:recommended', 'prettier'],
   env: {
     es2022: true,
     browser: true,
