f2ebac886528b9e1d61b70f93af92ae713f9f5f9
Abbe Keultjes <flauwekeul@gmail.com>
2022-08-09 12:35:05 +0200

    :white_check_mark: add tests for translate()

---
+++ src/hex/functions/translate.test.ts
@@ -0,0 +1,19 @@
+import { expect, test } from 'vitest'
+import { Hex } from '../hex'
+import { translate } from './translate'
+
+test('returns a clone of the passed hex with the delta partial cube coordinates', () => {
+  const hex = new Hex()
+  const result = translate(hex, { q: -1, r: 1 })
+
+  expect(result).toBeInstanceOf(Hex)
+  expect(result).toContain({ q: -1, r: 1, s: 0 })
+})
+
+test('returns translated cube coordinates from partial cube coordinates', () => {
+  const input = { r: 2, s: -3 } // resolves to { q: 1, r: 2, s: -3 }
+  const delta = { q: -3, s: 1 } // resolves to { q: -3, r: 2, s: 1 }
+  const result = translate(input, delta)
+
+  expect(result).toEqual({ q: -2, r: 4, s: -2 })
+})
--- src/hex/functions/translate.ts
+++ src/hex/functions/translate.ts
@@ -1,7 +1,6 @@
 import { Hex } from '../hex'
 import { CubeCoordinates, PartialCubeCoordinates } from '../types'
 import { completeCube } from './completeCube'
-import { toCube } from './toCube'
 
 /**
  * @category Hex
@@ -12,13 +11,9 @@ export function translate<T extends Hex>(
   input: T | PartialCubeCoordinates,
   delta: PartialCubeCoordinates,
 ): T | CubeCoordinates {
-  const { q: deltaQ, r: deltaR, s: deltaS } = completeCube(delta)
-
-  if (input instanceof Hex) {
-    const { q, r, s } = toCube(input, input)
-    return input.clone({ q: q + deltaQ, r: r + deltaR, s: s + deltaS })
-  }
-
   const { q, r, s } = completeCube(input)
-  return { q: q + deltaQ, r: r + deltaR, s: s + deltaS }
+  const { q: deltaQ, r: deltaR, s: deltaS } = completeCube(delta)
+  const translation = { q: q + deltaQ, r: r + deltaR, s: s + deltaS }
+
+  return input instanceof Hex ? input.clone(translation) : translation
 }
