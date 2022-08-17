aeccb1a53ecb348ce15067df97429d8c20b1b5ca
Abbe Keultjes <flauwekeul@gmail.com>
2022-08-17 22:34:12 +0200

    :wrench: add and configure @semantic-release/git

--- .releaserc.js
+++ .releaserc.js
@@ -1,3 +1,15 @@
 module.exports = {
   extends: ['semantic-release-config-gitmoji'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['dist/**/*.{js,mjs,ts}', 'docs', 'package.json'],
+        message: ':bookmark: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
 }
